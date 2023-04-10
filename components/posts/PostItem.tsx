965aeee64ed4dbdf727a3158f0f9d08a248f156c
Abdullah All Noman <MD Noman Hossain>
2023-04-10 11:33:46 +0600

    postfeed change

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -59,8 +59,10 @@ const PostItem: React.FC<PostItemProps> = ({ userId, data }) => {
 
     return (
         <div onClick={goToPost} className='border-b-[1px] border-neutral-800 p-5 cursor-pointer hover:bg-neutral-900 transition'>
-            <div className='flex flex-row items-center justify-between gap-3 '>
-                <Avatar userId={data.user.id} />
+            <div className='flex flex-row items-center gap-3 '>
+                <div className='w-12'>
+                    <Avatar userId={data.user.id} />
+                </div>
                 <div >
                     <div className='flex flex-row items-center gap-2'>
                         <p onClick={goToUser} className='text-white font-semibold cursor-pointer hover:underline'>{data.user.name}</p>
